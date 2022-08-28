712abcdd4751e48e8ecd7934ea60893eed8dec5d
akinari tsugo <garafo@gmail.com>
2022-08-28 14:51:36 +0900

    Add sample secrets

--- 10_infra/main.tf
+++ 10_infra/main.tf
@@ -23,8 +23,8 @@ terraform {
 provider "aws" {
   profile = "terraform"
   region  = "ap-northeast-1"
-  # access_key = "AKIAIOSFODNN7EXAMPLe"
-  # secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLeKEY"
+  # access_key = "AKIAIOSFODNN7EXAMPLE"
+  # secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 }
 
 provider "aws" {
